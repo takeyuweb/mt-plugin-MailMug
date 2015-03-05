eeeac1fb65640642b545f946f6a143c57fe61a17
Yuichi Takeuchi <uzuki05@takeyu-web.com>
2015-03-05 13:47:24 +0900

    注意書き追加

--- plugins/MailMug/lib/MailMug/L10N/ja.pm
+++ plugins/MailMug/lib/MailMug/L10N/ja.pm
@@ -20,6 +20,7 @@ our %Lexicon = (
     'Importing ... (already completed up to [_1])' => 'インポートしています…（[_1]件目まで完了済み）',
     'Imported the [_1] record. (Of which [_2] number Skip)' => '[_1]件のレコードをインポートしました。（うち[_2]件をスキップ）',
     'Check the list of subscripters.' => '購読者の一覧を確認する',
+    'Existing users will not be overwritten.' => '既存のユーザー情報は上書きされません。',
 );
 
 1;
